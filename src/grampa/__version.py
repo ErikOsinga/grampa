ad427b75a4694ba6edf33522ae5c915294285112
ErikOsinga <erikosinga876@hotmail.com>
2025-06-15 15:40:35 +0200

    fix versioning

--- src/grampa/__version.py
+++ src/grampa/__version.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 version: str
 version_tuple: tuple[int, int, int] | tuple[int, int, int, str, str]
 
-__version__ = version = "0.0.6"
+__version__ = version = "0.0.7"
 __version_tuple__ = version_tuple = (0, 0, 6, "", "")
